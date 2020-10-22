b7246e23c19fe318b28c29d05877badc8bb0a1cf
PodolakM <mati.pooo@gmail.com>
2020-10-22 21:24:17 +0200

    Zadanie 2, start zadanie 3

---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+    public float Speed = 2.0f;
+    bool _way = true;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.x <= 10 && _way)
+        {
+            transform.Translate(Speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            _way = false;
+            transform.Translate(Speed * Time.deltaTime * -1, 0, 0);
+            if (transform.position.x <= 0 && !_way)
+            {
+                _way = true;
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovingCubeSquare.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCubeSquare : MonoBehaviour
+{
+    public float Speed = 2f;
+    public float Step = 2f;
+    private float _x;
+    private static readonly int[] WayList = new[] {0, 1, 2, 3};
+    private int _way = WayList[0];
+    Quaternion target = Quaternion.Euler(0, -90, 0);
+    private bool _rotate = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+        if ((transform.position.x <= 10 && _way == 0) || (transform.position.z <= 10 && _way == 1))
+        {
+            transform.Translate(Speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            if (!_rotate && transform.eulerAngles.y > 270 || Math.Abs(transform.eulerAngles.y) < 0.1)
+            {
+                
+                transform.rotation =
+                    Quaternion.Slerp(transform.rotation, target, Time.deltaTime * Step);
+            }
+            else
+            {
+                _rotate = true;
+                transform.Translate(0, 0, Speed * Time.deltaTime);
+
+            }
+        }
+
+    }
+}
