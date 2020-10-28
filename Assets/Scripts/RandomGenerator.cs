f430f0ccf623b71cf1af0935deaae4bf06e0ef86
PodolakM <mati.pooo@gmail.com>
2020-10-28 20:47:01 +0100

    Zadanie 5

---
+++ Assets/Scripts/RandomGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomGenerator : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (count != 10)
+        {
+            Vector3 position = new Vector3(Random.Range(0f, 9.0f), 1.1f, Random.Range(-9.0f, 0f));
+            if (!Physics.CheckBox(position, new Vector3(1f, 1f, 1f)))
+            {
+                Instantiate(obstacle, position, Quaternion.identity);
+                count++;
+            }
+        }
+    }
+
+}
