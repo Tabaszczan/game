4ba777b807eb806f27923cdbd71421e498e2d601
PodolakM <mati.pooo@gmail.com>
2020-11-05 17:48:13 +0100

    Zadanie 3 i 4 i 5

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public Transform player;
+
+    public float sensitivity = 200f;
+
+    private float xRotation = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        player.Rotate(Vector3.up * mouseXMove);
+
+        xRotation -= mouseYMove;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+       transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+       transform.Rotate(new Vector3(-mouseYMove, 0f, 0f), Space.Self);
+
+
+
+    }
+}
